9bdc4d8c897110eb6f607b64a932a8a06ea46bb0
Gpasha <g1ebikpasha@gmail.com>
2018-06-01 19:35:00 +0300

    active

--- app/js/my.js
+++ app/js/my.js
@@ -181,6 +181,7 @@ $(document).ready(function () {
 
 });
 
+
 $(window).load(function () {
     // Группы объектов
     var groups = [
@@ -286,34 +287,3 @@ $(window).load(function () {
     }
 });
 
-
-// Делаем анимацию блоков для мобильных устройств.
-
-$(window).on('load', function () {
-
-    var $itemSmall = (".containerH1-itemSmall");
-
-    var $itemSmallTypes = (".containerH1-itemSmall__types");
-    var $itemSmallTypesH1 = (".containerH1-itemSmalltypes a h1");
-    var $itemSmallTypesP = (".containerH1-itemSmal__types p");
-    var $itemSmallTransform = (".containerH1-itemSmall__types .transform-know-more-itemSmall");
-
-    <!-- var $itemSmallTypesJS = (".itemSmallTypesJS");
-    var $itemSmallTypesH1JS = (".itemSmallTypesH1JS");
-    var $itemSmallTypesPJS = (".itemSmallTypesPJS");
-    var $itemSmallTransformJS = (".itemSmallTransformJS");
- -->
-    $itemSmall.on('touch', function () {
-
-        $itemSmallTypes.toggleClass("itemSmallTypesJS");
-
-        $itemSmallTypesH1.toggleClass("itemSmallTypesH1JS");
-
-        $itemSmallTypesP.toggleClass("itemSmallTypesPJS");
-
-        $itemSmallTransform.toggleClass("itemSmallTransformJS");
-
-        return false;
-
-    });
-});
--- dist/js/main.js
+++ dist/js/main.js
@@ -18629,6 +18629,7 @@ $(document).ready(function () {
 
 });
 
+
 $(window).load(function () {
     // Группы объектов
     var groups = [
@@ -18732,36 +18733,4 @@ $(window).load(function () {
         // Выставляем масштаб карты чтобы были видны все группы.
         myMap.setBounds(myMap.geoObjects.getBounds());
     }
-});
-
-
-// Делаем анимацию блоков для мобильных устройств.
-
-$(window).on('load', function () {
-
-    var $itemSmall = (".containerH1-itemSmall");
-
-    var $itemSmallTypes = (".containerH1-itemSmall__types");
-    var $itemSmallTypesH1 = (".containerH1-itemSmalltypes a h1");
-    var $itemSmallTypesP = (".containerH1-itemSmal__types p");
-    var $itemSmallTransform = (".containerH1-itemSmall__types .transform-know-more-itemSmall");
-
-    <!-- var $itemSmallTypesJS = (".itemSmallTypesJS");
-    var $itemSmallTypesH1JS = (".itemSmallTypesH1JS");
-    var $itemSmallTypesPJS = (".itemSmallTypesPJS");
-    var $itemSmallTransformJS = (".itemSmallTransformJS");
- -->
-    $itemSmall.on('touch', function () {
-
-        $itemSmallTypes.toggleClass("itemSmallTypesJS");
-
-        $itemSmallTypesH1.toggleClass("itemSmallTypesH1JS");
-
-        $itemSmallTypesP.toggleClass("itemSmallTypesPJS");
-
-        $itemSmallTransform.toggleClass("itemSmallTransformJS");
-
-        return false;
-
-    });
 });
